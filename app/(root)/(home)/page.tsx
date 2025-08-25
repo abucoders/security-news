3297947ed1b2e693fcae02478c4fbe22be0e4472
abucoders <abdulloyev.dev@gmail.com>
2025-08-25 11:09:38 +0500

    shadcn-ui init

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 const Page = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="p-3">
+      <h1 className="text-3xl">Home Page!</h1>
+      <Button>Home</Button>
+    </div>
+  );
 };
 
 export default Page;
